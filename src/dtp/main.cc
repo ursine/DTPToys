7000a05ac99b268a05dbf75c156672c524302ea2
Gary Coulbourne <bear@bears.org>
2020-03-14 15:43:52 -0400

    starting to try wayland client

--- src/dtp/main.cc
+++ src/dtp/main.cc
@@ -1,6 +1,24 @@
+#include <cstdio>
+#include <wayland-client.h>
+
 
 
 int main(int argc, char* argv[])
 {
+  struct wl_display* display = NULL;
+
+  display = wl_display_connect(NULL);
+
+  if (display == NULL) {
+    fprintf(stderr, "Can't connect to display\n");
+    exit(1);
+  }
+  printf("connected to display\n");
+  
+  wl_display_disconnect(display);
+  printf("disconnected from display\n");
+
   return 0;
 }
+
+
